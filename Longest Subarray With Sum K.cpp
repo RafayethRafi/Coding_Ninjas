e40e64f845584c1a89c2289651e592993ade0852
RafayethRafi <87166899+RafayethRafi@users.noreply.github.com>
2023-04-06 13:20:25 +0600

    Create Longest Subarray With Sum K.cpp
    https://www.codingninjas.com/codestudio/problems/longest-subarray-with-sum-k_6682399?utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_Arrayproblems&leftPanelTab=0

---
+++ Longest Subarray With Sum K.cpp
@@ -0,0 +1,23 @@
+int longestSubarrayWithSumK(vector<int> a, long long k) {
+    int l =0,r =0,i,maxLen =0;
+    int n = a.size();
+    long long sum = a[0];
+
+    while(r<n)
+    {
+        while(l<=r && sum>k)
+        {
+            sum -= a[l];
+            l++;
+        }
+        if(sum==k)
+        {
+            maxLen = max(maxLen,(r-l + 1));
+        }
+        r++;
+        if(r<n) sum += a[r];
+    }
+
+    return maxLen;
+
+}
