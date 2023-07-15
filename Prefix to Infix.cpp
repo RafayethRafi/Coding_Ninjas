4713d85acaa335eccd3f0694553bb71b9be35fc9
RafayethRafi <87166899+RafayethRafi@users.noreply.github.com>
2023-07-15 20:29:01 +0600

    Create Prefix to Infix.cpp
    https://www.codingninjas.com/studio/problems/prefix-to-infix_1215000?utm_source=striver&utm_medium=website&utm_campaign=a_zcoursetuf&leftPanelTab=0

---
+++ Prefix to Infix.cpp
@@ -0,0 +1,27 @@
+string prefixToInfixConversion(string &s){
+	stack<string> st;
+
+	string ans;
+	for(int i=s.size()-1;i>=0;i--)
+	{
+		if(s[i]=='+'||s[i]=='-'||s[i]=='*'||s[i]=='/'||s[i]=='^')
+		{
+			string x = st.top();
+			st.pop();
+			string y = st.top();
+			st.pop();
+
+			ans = '('+x+s[i]+y+')';
+
+			st.push(ans);
+		}
+		else
+		{
+			string k = "";
+			k+= s[i];
+			st.push(k);
+		}
+	}
+
+	return ans;
+}
